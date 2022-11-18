2f302c9881f923cc9f4427d60a02d08b76346457
iptton <iptton@gmail.com>
2022-11-18 20:12:31 +0800

    feat: add config for android/iOS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,16 @@ pluginManagement {
 }
 
 rootProject.name = "ksvg"
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven( url = "https://mirrors.tencent.com/repository/maven/tencent_public" )
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.3")
+    }
+}
---
+++ src/androidMain/kotlin/com/github/nwillc/ksvg/logging/log.kt
@@ -0,0 +1,4 @@
+package com.github.nwillc.ksvg.logging
+
+internal actual fun log(logLevel: LogLevel, message: String) {
+}
